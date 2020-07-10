5048787caaa2439104eb7385c4eacdb12cd98178
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2020-07-10 01:31:41 -0400

    [feat] complete Stage 2
    Get the number of cups of coffee the user needs, then output the amounts of water, milk and coffee beans needed.

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,13 +1,21 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        int WATER = 200;    // ml of water need to make a cup
+        int MILK = 50;      // ml of milk needed for a cup
+        int BEANS = 15;     // g of coffee beans
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+
+        System.out.printf("For %d cups of coffee you will need:\n", cups);
+        System.out.printf("%d ml of water\n", cups * WATER);
+        System.out.printf("%d ml of milk\n", cups * MILK);
+        System.out.printf("%d g of coffee beans\n", cups * BEANS);
     }
 }
